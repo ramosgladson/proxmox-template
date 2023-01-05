fc22683f441a37046bc09e336d183f1b839cc052
ramosgladson <gladson.ramos@hotmail.com>
2023-01-04 20:56:18 -0400

    add functions

--- vmprepare.sh
+++ vmprepare.sh
@@ -1,24 +1,50 @@
 #!/bin/bash
 
-truncate -s 0 /etc/machine-i
-truncate -s 0 /var/lib/dbus/machine-id
+
+#### Function to check errors
+check_errors() {
+	if [ $? -ne 0 ] ; then
+		echo "[FAIL] - $ACTION"
+		exit
+	else
+		echo "[OK] - $ACTION"
+	fi
+}
+
+
+ACTION="Truncate machine id" 
+truncate -s 0 /etc/machine-i && truncate -s 0 /var/lib/dbus/machine-id > /dev/null 2>&1
+check_errors
 
 
 # Debian/Ubuntu
 
-apt-get install qemu-guest-agent
+ACTION="Install qemu agent"
+apt-get install qemu-guest-agent > /dev/null 2>&1
+check_errors
+
 
 # Redhat based
 #yum install qemu-guest-agent
 
-systemctl start qemu-guest-agent
+ACTION="Start qemu agent"
+systemctl start qemu-guest-agent > /dev/null 2>&1
+check_errors
 
-systemctl start qemu-guest-agent
+ACTION="Cleaning ssh_host"
+rm /etc/ssh/ssh_host_* > /dev/null 2>&1
+check_errors
 
-rm /etc/ssh/ssh_host_*
+ACTION="Cloud init install and clean"
+apt-get install cloud-init && cloud-init clean > /dev/null 2>&1
+check_errors
 
-apt-get install cloud-init
+ACTION="Shutting down"
+sleep 2 > /dev/null 2>&1
+check_errors
 
-cloud-init clean
+sudo rm -r ../proxmox-template
 
+sleep 2
 
+sudo hutdown -h now
